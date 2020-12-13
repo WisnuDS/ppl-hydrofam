ba5a4b6f54cc2125f2577de1bbdab7301ab8c9c3
WisnuDs <182410103009@cs.unej.ac.id>
2020-12-13 10:41:30 +0700

    fix some fuckin bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,12 +79,11 @@
 <!-- loader -->
 <script>window.token = "{{csrf_token()}}";</script>
 <script>window.guest = "{{auth()->guest()}}";</script>
-<script src="{{mix('js/app.js')}}"></script>
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
         <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
         <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
                 stroke="#F96D00"/></svg></div>
-
+<script src="{{mix('js/app.js')}}"></script>
 <script src="{{ asset('js/jquery.min.js') }}"></script>
 <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
 <script src="{{ asset('js/popper.min.js') }}"></script>
@@ -100,6 +99,7 @@
 <script src="{{ asset('js/scrollax.min.js') }}"></script>
 <script src="{{ asset('js/google-map.js') }}"></script>
 <script src="{{ asset('js/main.js') }}"></script>
+
 @stack('scripts_addons')
 @jquery
 @toastr_js
